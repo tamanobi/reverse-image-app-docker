98b44fbede6d7948f690a0bc4355e8f3074c378e
Yasu <yasu@pixiv.co.jp>
2018-06-26 23:07:29 +0900

    fix

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-from keras.applications.vgg16 import VGG16
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
 import numpy as np
 from model import model
-from flask import Flask
+from flask import Flask, request, jsonify
+import io
+from PIL import Image
 
 app = Flask(__name__)
-model = VGG16(weights='imagenet', include_top=False, pooling='avg')
 
 @app.route("/", methods=['POST'])
 def extract():
-    img = image.load_img(img_path, target_size=(224, 224))
+    r = request.get_data()
+    img = image.load_img(io.BytesIO(r), target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
     features = model.predict(x)
-    return str(feature.shape)
+    return jsonify({'feature': features.tolist()[0]})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
--- model.py
+++ model.py
@@ -1,3 +1,10 @@
+import numpy as np
 from keras.applications.vgg16 import VGG16
 
 model = VGG16(weights='imagenet', include_top=False, pooling='avg')
+
+# see http://tsuwabuki.hatenablog.com/entry/2016/10/17/150033
+x = np.zeros((224, 224, 3))
+x = np.expand_dims(x, axis=0)
+model.predict(x)
+
