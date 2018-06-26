2c5d41eb1b135ac530f7fe0378eee8b992487b7c
Yasu <yasu@pixiv.co.jp>
2018-06-26 23:21:07 +0900

    create view

--- app.py
+++ app.py
@@ -8,15 +8,30 @@ from PIL import Image
 
 app = Flask(__name__)
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=['GET', 'POST'])
 def extract():
-    r = request.get_data()
-    img = image.load_img(io.BytesIO(r), target_size=(224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    features = model.predict(x)
-    return jsonify({'feature': features.tolist()[0]})
+    if request.method == 'GET':
+        return """
+<html>
+<body>
+<h1>特徴量抽出器</h1>
+<form method="POST" enctype="multipart/form-data">
+<input type="file" name="image">
+<button type="submit">submit</button>
+</form>
+</body>
+</html>
+"""
+    elif request.method == 'POST':
+        r = request.files['image']
+        # r = request.get_data()
+        img = image.load_img(r, target_size=(224, 224))
+        # img = image.load_img(io.BytesIO(r), target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        features = model.predict(x)
+        return jsonify({'feature': features.tolist()[0]})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
